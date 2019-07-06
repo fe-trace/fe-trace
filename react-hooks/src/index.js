f8ea41b72e2df4c31ee289e39ca470d920c030cc
jinchuanjun <jinchuanjun@cd.tuan800.com>
2019-07-06 11:58:46 +0800

    init

---
+++ react-hooks/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App(props) {
+    return (
+        <div>app</div>
+    );
+}
+
+render(<App />, document.querySelector("#app"));
